d50343deda4c4484a7451658f2368bf473118d17
kdbrown5 <46332316+kdbrown5@users.noreply.github.com>
2020-01-17 22:53:51 -0800

    Update sync.py

--- sync.py
+++ sync.py
@@ -16,65 +16,72 @@ from datetime import datetime, timedelta
 today = datetime.today().strftime('%Y-%b-%d')
 yesterday = datetime.today() - timedelta(1)
 yesterday = yesterday.strftime('%Y-%b-%d')
+tomorrow = datetime.today() + timedelta(1)
+tomorrow = tomorrow.strftime('%Y-%b-%d')
 
-pp = pprint.PrettyPrinter(indent=4).pprint
+#pp = pprint.PrettyPrinter(indent=4).pprint
 
 chrome_options = Options()
 #chrome_options.add_extension("proxy.zip")
 chrome_options.add_argument("--incognito")
-driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path='/home/kevin/scripts/selenium/chromedriver', chrome_options=chrome_options)
 
-url = 'https://site.com/dl'
-url2 = 'https://site.com/dl2'
-url3 = 'https://site.com/dl3'
+url = 'https://xxxxxxxxxxxxxxxxxxxxxxxx'
+url2 = 'xxxxxxxxxxxxxxxxxx'
+url3 = 'xxxxxxxxxxxxxxxxxx'
+
 driver.get(url)
 driver.get(url3)
 page = driver.page_source
 soup = bs4(page, 'html.parser')
 
+regex2 = re.findall(r"(.*href)*=\"(.*)(mkv).*(.mkv\")(.*)(\d{4}-[a-zA-Z]{3}-\d{2} \d{2}:\d{2})", page)
+
 tables = soup.findChildren('table')
 my_table = tables[0]
 rows = my_table.findChildren(['tr'])
 dates = []
+
 for row in rows:
 	cells = row.findChildren('td')
 	for cell in cells:
 		value = cell.string
 		dates.append(value)
 
-newdownload = []
-for row in rows:
-	cells = row.findChildren('td')
-	for cell in cells:
-		if '2020-Jan-04' in str(cells):
-			newdownload.append(cell)
+dldates = [k for k in dates if (str(yesterday)) in k]
+dldates1 = [k for k in dates if (str(today)) in k]
+dldates2 = [k for k in dates if str(tomorrow) in k]
+dldates3 = (dldates + dldates1 + dldates2)
 
+x2 = []
 
-dldates = [k for k in dates if '2020' in k]
+for items in regex2:
+	for datez in dldates3:
+		if str(items[5]) == datez:
+			x2.append(str(items[1]) + 'mkv')
 
-servermkv = [a['href'] for a in soup.find_all('a', href=True)]
-servermkv = sorted(servermkv)
-del servermkv[0:8]
-
-
-with open('files.txt', 'w') as filehandle:
-    for listitem in servermkv:
-        filehandle.write('%s\n' % listitem)
-
-pcfiles = os.listdir("/python")
+pcfiles = os.listdir("/home/kevin/Downloads")
 pcfiles = sorted(pcfiles)
 
-downloadmkv = list(set(servermkv) - set(pcfiles))
+downloadmkv = list(set(x2) - set(pcfiles))
 downloadmkv = sorted(downloadmkv)
 
-with open('pcfiles.txt', 'w') as filehandle:
-    for listitem in pcfiles:
-        filehandle.write('%s\n' % listitem)
-
-with open('downloadmkv.txt', 'w') as filehandle:
-    for listitem in downloadmkv:
-        filehandle.write('%s\n' % listitem)
+for itemz in downloadmkv:
+	itemz = ('https://xxxxxxxx/Extracted/' + itemz)
+	driver.get(itemz)
 
 
-res = [ele for ele in dldates if(ele in rows)] 
-#print(soup)
+def download_wait(path_to_downloads):
+    seconds = 0
+    dl_wait = True
+    while dl_wait and seconds < 20:
+        time.sleep(1)
+        dl_wait = False
+        for fname in os.listdir(path_to_downloads):
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+        seconds += 1
+    return seconds
+	
+driver.close()
+
